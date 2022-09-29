c205e0d74daf50f8c4df6f2555d1d81bfa85fbb5
Brendon Rodrigo Alcântara <92334562+BrendonAlc@users.noreply.github.com>
2022-09-29 00:01:42 -0300

    Adicionado Classe ContaCorrente e criado comparacão de saque, deposito e saldo.

---
+++ Bank/ContaCorrente.cs
@@ -0,0 +1,35 @@
+namespace bytebank
+{
+    public class ContaCorrente
+    {
+        public string titular;
+        public string conta;
+        public int numero_agencia;
+        public string nome_agencia;
+        public double saldo;
+
+        public bool Sacar(double valor) //método para verificar saldo, saque e após o saque
+        {
+            if(saldo < valor)
+            {
+                return false;
+            }
+            if(valor < 0)
+            {
+                return false;
+            }
+            else
+            {
+                saldo = saldo - valor;
+                return true;
+            }
+        }
+
+        public void Depositar(double valor)
+        {
+            saldo = saldo + valor;
+
+        }
+    }
+    
+}
--- Bank/Program.cs
+++ Bank/Program.cs
@@ -1,21 +1,53 @@
 ﻿using System;
+using bytebank;
 
-class Programa{
+class Programa
+{
     static void Main(string[] args)
     {
-        Console.WriteLine("Boas Vindas ao seu banco, Bank!");
+        Console.WriteLine("Boas Vindas ao seu banco, AlcBank!");
 
-        string titular = "Brendon Alcantara";
-        string conta = "10123-x";
-        int numero_agencia = 23;
-        string nome_agencia = "Agencia Central";
-        double saldo = 93.50;
+        
+        ContaCorrente conta1 = new ContaCorrente();
+        conta1.titular = "Brendon";
+        conta1.conta = "123-x";
+        conta1.numero_agencia = 23;
+        conta1.nome_agencia = "Agencia Central";
+        conta1.saldo = 100;
 
-        Console.WriteLine("Titular : " + titular);
-        Console.WriteLine("Conta: " + conta);
-        Console.WriteLine("Número da agência: " + numero_agencia);
-        Console.WriteLine("Nome da Agencia: " + nome_agencia);
-        Console.WriteLine("Saldo: " + saldo);
+        ContaCorrente conta2 = new ContaCorrente();
+        conta2.titular = "Luciana";
+        conta2.conta = "145-x";
+        conta2.numero_agencia = 15;
+        conta2.nome_agencia = "Agencia Central";
+        conta2.saldo = 100;
+
+        Console.WriteLine("Saldo da Luciana pré-saque: " + conta2.saldo);
+
+        bool saque = conta2.Sacar(30);
+        Console.WriteLine("Saque realizado? " + saque);
+
+        Console.WriteLine("Saldo da Luciana pós-saque: " + conta2.saldo);
+
+        conta2.Depositar(60);
+
+        Console.WriteLine("Saldo da Luciana pós-depósito " + conta2.saldo);
+
+
+        //Console.WriteLine($"Titular : {conta.titular}");
+        //Console.WriteLine($"Conta: {conta.conta}");
+        //Console.WriteLine($"Número da agência: {conta.numero_agencia}");
+        //Console.WriteLine($"Nome da Agencia: {conta.nome_agencia}");
+        //.WriteLine($"Saldo: {conta.saldo}");
+
+
+        /*Comparacao de saldo com o saldo da conta1 objeto
+        double saldo = 100;
+        double saldo2 = 100;
+        Console.WriteLine(saldo == conta1.saldo); */
+
+
+        Console.ReadKey();
     }
 }
 
